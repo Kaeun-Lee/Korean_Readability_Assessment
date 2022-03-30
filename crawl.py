045b6aa43d2016c19562f7a2a48474777124c65d
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-30 13:27:59 +0900

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -29,3 +29,31 @@ for file_num in tqdm(range(217,1885)):
 with open("Korean Sustainability Report RA Dataset.bin","wb") as fw:
     pickle.dump(df, fw)
 
+
+import pandas as pd
+from tqdm import tqdm
+from pororo import Pororo
+spacing = Pororo(task="gec", lang="ko")
+
+df = pd.read_excel('Korean Readability Assessment.xlsx')
+
+def ko_spacing(sent):
+    try:
+        if len(sent) <512:
+            return spacing(sent)
+        else: 
+            return spacing(sent[:512]) + spacing(sent[:512:])
+    except:
+        return '-'
+    
+file_contentCleansed = []
+
+for content in tqdm(df['file_content'].values):
+    content = content.split('. \n')[1:]
+    content = ' ' .join([_.replace('\n','') for _ in content])
+    content = [ko_spacing(_+'.') for _ in content.split('.') if _.endswith('다')]
+    content = ''.join(content)
+    file_contentCleansed.append(content)
+    
+df['file_contentCleansed'] = file_contentCleansed 
+df.to_excel('B_Korean Readability Assessment.xlsx')
