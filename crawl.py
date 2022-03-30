e980f08f807564594e5d6ee970ad3900cbd01461
GUIJIN SON <spthsrbwls123@yonsei.ac.kr>
2022-03-31 00:43:25 +0900

    Update crawl.py

--- crawl.py
+++ crawl.py
@@ -29,31 +29,21 @@ for file_num in tqdm(range(217,1885)):
 with open("Korean Sustainability Report RA Dataset.bin","wb") as fw:
     pickle.dump(df, fw)
 
-
 import pandas as pd
 from tqdm import tqdm
-from pororo import Pororo
-spacing = Pororo(task="gec", lang="ko")
+from pororo.tasks.grammatical_error_correction import PororoGecFactory,PororoTransformerGec
+import math 
 
-df = pd.read_excel('Korean Readability Assessment.xlsx')
+spacing = PororoGecFactory(task="gec",lang="ko",model="charbert.base.ko.spacing").load('cuda:0')
 
-def ko_spacing(sent):
-    try:
-        if len(sent) <512:
-            return spacing(sent)
-        else: 
-            return spacing(sent[:512]) + spacing(sent[:512:])
-    except:
-        return '-'
-    
-file_contentCleansed = []
 
-for content in tqdm(df['file_content'].values):
-    content = content.split('. \n')[1:]
-    content = ' ' .join([_.replace('\n','') for _ in content])
-    content = [ko_spacing(_+'.') for _ in content.split('.') if _.endswith('다')]
-    content = ''.join(content)
-    file_contentCleansed.append(content)
-    
-df['file_contentCleansed'] = file_contentCleansed 
-df.to_excel('B_Korean Readability Assessment.xlsx')
+def tp_sentenceCleanse(input_sentence:str, func, upper_bound=512):
+    output_sentence_space = ''
+    tot_block = math.ceil(len(input_sentence)/upper_bound)
+    for _ in range(tot_block):
+        s = _ * upper_bound
+        e = (_+1) * upper_bound
+        output_sentence = input_sentence[s:e]
+        output_sentence = func(output_sentence)
+        output_sentence_space += output_sentence
+    return output_sentence_space
