7e45414de1445919c9793d7af0b8874cc6cc318f
guijinSON <spthsrbwls123@yonsei.ac.kr>
2022-04-02 22:28:23 +0900

    update ra_fogIndex

--- ra_func.py
+++ ra_func.py
@@ -1,14 +1,42 @@
 from tqdm import tqdm
 import numpy as np
-from pororo import Pororo
-sa = Pororo(task="sentiment", model="brainbert.base.ko.nsmc", lang="ko")
+from pororo.tasks.sentiment_analysis import PororoSentimentFactory
+from pororo.tasks.tokenization import PororoTokenizationFactory
 
-def ra_sentimentAssesment(input_sentence:str,func):
+def ra_sentimentAssesment(input_sentence:str, device='cpu'):
+    func = PororoSentimentFactory(task="sentiment", lang="ko", model="brainbert.base.ko.shopping").load(device)
     ra_sentimentAssesment = []
 
-    for sent in tqdm(input_sentence.split('.')):
-        o = func(sent,show_probs=True)
-        ra_sentimentAssesment.append(o['positive'])
+    for sent in input_sentence.split('.'):  # 문장 단위로 쪼개기
+        if len(sent) > 512:                 # pororo 라이브러리 특성상 최대 토큰 수 512, 이상으로 길 경우 512에서 truncate
+            sent = sent[:512]
+        o = func(sent,show_probs=True)         
+        ra_sentimentAssesment.append(o['positive']) # o['negative'] = 1 - o['positive'] 라 내재 변동성 계산하면 positive/negative 둘 중 무엇을 사용해도 값이 동일함
     
     ra_sentimentAssesment = np.array(ra_sentimentAssesment).std() * (len( ra_sentimentAssesment)**0.5)
     return ra_sentimentAssesment
+
+def ra_fogIndex(input_sentence:str, n:int, device='cpu'):
+    func = PororoTokenizationFactory(task="tokenization", lang="ko", model="bpe32k.ko").load(device)
+    input_sentence_length = len(input_sentence.split('.'))  # 문장 수
+    input_sentence_tokenzied = func(input_sentence)    # 토큰화
+
+    num_letter = 0  # 글자 수
+    long_words = 0  # n음절 이상인 단어 수
+    total_word = 0  # 단어 수
+
+    for word in input_sentence_tokenzied:   # 단어 수는 빈칸(" ")으로 구분
+        word = re.sub(r'▁', '', word)       # _ 제거
+        word = re.sub(r'\.', '', word)      # . 제거
+        
+        if word != '':
+            num_letter += len(word)
+            if len(word) == n:
+                long_words += 1
+            if len(word) >= 2:
+                total_word += 1
+
+    ASL = total_word / input_sentence_length  # 평균 문장 길이
+    fog_index = (ASL + (long_words / total_word) * 100) * 0.4
+    
+    return fog_index
